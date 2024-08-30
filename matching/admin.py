7f07bdd2e458eee2b8a44198c2d939b165e78a7e
Rindra Hosea MAHEFANIAINA <mahefaniainarindra@gmail.com>
2024-08-30 06:38:41 +0300

    register model in admin site

--- matching/admin.py
+++ matching/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from matching.models import (
+    HardSkill,
+    JobHardSkill,
+    JobOffer,
+    JobSoftSkill,
+    SoftSkill,
+    User,
+    UserHardSkill,
+    UserSoftSkill,
+)
+
 # Register your models here.
+admin.site.register(
+    User,
+    JobOffer,
+    HardSkill,
+    SoftSkill,
+    UserHardSkill,
+    UserSoftSkill,
+    JobHardSkill,
+    JobSoftSkill,
+)
