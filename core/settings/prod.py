6fd5b474765ec6f01b91a5d08a2261becaa4eed5
Rindra Hosea MAHEFANIAINA <mahefaniainarindra@gmail.com>
2024-08-30 06:47:40 +0300

    fix csrf_trusted_orgins

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -7,4 +7,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = "staticfiles"
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    f"http://{os.getenv("ALLOWED_HOST")}"
+]
